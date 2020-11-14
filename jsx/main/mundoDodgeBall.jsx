aedbce359707b93e0947bd9fb2b1f7ed3c698907
Cheon seungmin <dudghsx12@gmail.com>
2020-11-14 14:25:21 +0900

    add notes main.jsx 1

--- jsx/main/mundoDodgeBall.jsx
+++ jsx/main/mundoDodgeBall.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import axios from 'axios'
 import Main from './main.jsx'
 
+//#mundoDodgeBall을 찾아 Main 컴포넌트로 렌더링한다.
 ReactDOM.render(
     <Main/>,
     document.getElementById('mundoDodgeBall')
