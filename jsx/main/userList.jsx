4348e65b7b72e7bd1328f9c8cbcee75af448ebd5
Cheon seungmin <dudghsx12@gmail.com>
2020-11-14 15:35:58 +0900

    add notes userList.jsx 1

--- jsx/main/userList.jsx
+++ jsx/main/userList.jsx
@@ -2,25 +2,29 @@ import React from 'react'
 import axios from 'axios'
 import socketIo from 'socket.io-client'
 
+// 현재 접속 중인 사용자 목록을 알려주는 컴포넌트
 class userList extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			users: null,
-			page: 1,
-			list: 1
+			users: null, // 사용자 배열
+			page: 1, // 현재 페이지
+			list: 1 // 전체 페이지
 		}
 	}
 	userList() {
 		axios.get('/userList').then(function(res) {
 			this.setState({
-				users: res.data.users,
+				users: res.data.users, // 사용자 목록을 받아옴
+                // 사용자를 5명씩 보여줌, 5명으로 딱 나눠진다면 (ex 10명) 2페이지, 아니라면 (ex 11명) +1을 해서 3페이지가 된다.
 				list: ((res.data.users.length % 5) == 0)? ((res.data.users.length / 5)): (parseInt(res.data.users.length / 5) + 1)
 			})
 		}.bind(this))
 	}
 	
+    // 원하는 사용자를 초대하는 함수
 	invite(event) {
+        // 서버에 클릭한 event의 정보를 전송하여 상대방과 연결을 요청한다.
 		axios.post('/invite', {
 			userId: this.props.state.userId,
 			socketId: this.props.state.socketId,
@@ -28,6 +32,7 @@ class userList extends React.Component {
 		})
 	}
 	
+    // 현재 -1 페이지를 보여줌
 	prePage() {
 		if(1 < this.state.page){
 			this.setState({
@@ -36,6 +41,7 @@ class userList extends React.Component {
 		}
 	}
 	
+    // 현재 +1 페이지를 보여줌
 	nextPage() {
 		if(this.state.page < this.state.list){
 			this.setState({
@@ -47,6 +53,7 @@ class userList extends React.Component {
 	render() {
 		return (
 			<div>
+<!--                사용자 목록 보기 버튼을 누르면 유저 목록을 서버로부터 받아와 출력한다.-->
 				<input type="submit" value="사용자 목록 보기" onClick={this.userList.bind(this)}></input><br/>
 				{(
 					()=>{
@@ -54,6 +61,7 @@ class userList extends React.Component {
 							return <div>
 										<div style={{height: '140px'}}>
 											{
+                                                // 현재 페이지의 5명을 출력한다. 만약 그 사용자가 gaming 상태라면 출력하지 않는다.
 												this.state.users.slice((this.state.page-1)*5, ((this.state.page-1)*5)+5).map((user, i) => {
 													if((user.userId != this.props.state.userId) && (user.userId != '') && (user.state != "gaming"))
 														return <div key={user.userId} style={{padding: 'padding: 5px 5px 5px 0px'}}>
@@ -66,6 +74,7 @@ class userList extends React.Component {
 											}
 										</div>
 										<div style={{verticalAlign:'bottom', textAlign: 'center'}}>
+<!--                                        페이지 이동 버튼들-->
 										<button onClick={this.prePage.bind(this)}>&lt;</button>
 										&nbsp; {this.state.page} / {this.state.list} &nbsp;
 										<button onClick={this.nextPage.bind(this)}>&gt;</button>
